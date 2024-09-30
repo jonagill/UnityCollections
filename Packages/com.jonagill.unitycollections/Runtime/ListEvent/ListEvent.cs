0b8bc29b7119584a59f9771e7c8ff39e893bd805
Jon Gill <jonagill@gmail.com>
2024-09-30 15:31:11 -0700

    Add ListEvent class and tests

---
+++ Packages/com.jonagill.unitycollections/Runtime/ListEvent/ListEvent.cs
@@ -0,0 +1,572 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace UnityCollections
+{
+    /// <summary>
+    /// Replacement for System.Action that is backed by a list.
+    /// System.Action is immutable, and so each modification requires re-allocating the entire array of subscribers,
+    /// which can be very expensive for global events or other events with many many subscribers.
+    /// By backing the collection with a list instead, we can pre-allocate and re-use memory rather than constantly
+    /// re-allocating the entire collection.
+    /// </summary>
+    public abstract class ListEventBase<T>
+    {
+        protected struct ModificationData
+        {
+            public enum Type
+            {
+                ADD,
+                REMOVE,
+                CLEAR
+            }
+
+            public Type type;
+            public T action;
+        }
+
+        private int iteratingCount;
+        private readonly bool clearAfterInvoke;
+        protected readonly bool useTryCatch;
+        protected List<T> actionList;
+
+        public bool HasSubcribers
+        {
+            get { return actionList != null && actionList.Count > 0; }
+        }
+        
+        private List<ModificationData> queuedModifications;
+
+        protected ListEventBase(bool useTryCatch, bool clearAfterInvoke)
+        {
+            this.useTryCatch = useTryCatch;
+            this.clearAfterInvoke = clearAfterInvoke;
+        }
+
+        protected bool BeginInvoke()
+        {
+            if (actionList == null)
+            {
+                return false;
+            }
+
+            lock (actionList)
+            {
+                iteratingCount++;
+            }
+
+            return true;
+        }
+
+        protected void EndInvoke()
+        {
+            lock (actionList)
+            {
+                // In case of reentrancy, only clear the list and process queued modifications
+                // when the original call finishes invoking.
+                if (iteratingCount == 1)
+                {
+                    if (clearAfterInvoke)
+                    {
+                        actionList.Clear();
+                    }
+
+                    ProcessQueuedModifications();
+                }
+                iteratingCount--;
+            }
+        }
+
+        protected void ProcessQueuedModifications()
+        {
+            if (queuedModifications == null)
+            {
+                return;
+            }
+
+            // Process all the actions in the order they came in
+            int count = queuedModifications.Count;
+            for (int i = 0; i < count; i++)
+            {
+                switch (queuedModifications[i].type)
+                {
+                    case ModificationData.Type.ADD:
+                        actionList.Add(queuedModifications[i].action);
+                        break;
+                    case ModificationData.Type.REMOVE:
+                        actionList.Remove(queuedModifications[i].action);
+                        break;
+                    case ModificationData.Type.CLEAR:
+                        actionList.Clear();
+                        break;
+                }
+            }
+
+            queuedModifications.Clear();
+        }
+
+        private static void EnsureListCreated<U>(ref List<U> list)
+        {
+            if (list == null)
+            {
+                // Use CompareExchange to ensure that only one version of the new List
+                // exists. If two callers attempt to do this at the same time on different threads, only
+                // one will successfully store the list in the assigned field.
+                Interlocked.CompareExchange(ref list, new List<U>(), null);
+            }
+        }
+
+        public void Add(T action, bool dontTrackForDebugCleanup = false)
+        {
+            if (action != null)
+            {
+                EnsureListCreated(ref actionList);
+
+                lock (actionList)
+                {
+                    if (iteratingCount > 0)
+                    {
+                        EnsureListCreated(ref queuedModifications);
+                        queuedModifications.Add(new ModificationData { type = ModificationData.Type.ADD, action = action });
+                    }
+                    else
+                    {
+                        actionList.Add(action);
+                    }
+                }
+            }
+        }
+
+        public void Remove(T action)
+        {
+            if (action != null && actionList != null)
+            {
+                lock (actionList)
+                {
+                    if (iteratingCount > 0)
+                    {
+                        EnsureListCreated(ref queuedModifications);
+                        queuedModifications.Add(new ModificationData { type = ModificationData.Type.REMOVE, action = action });
+                    }
+                    else
+                    {
+                        actionList.Remove(action);
+                    }
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            if (actionList != null)
+            {
+                lock (actionList)
+                {
+                    if (iteratingCount > 0)
+                    {
+                        EnsureListCreated(ref queuedModifications);
+                        queuedModifications.Add(new ModificationData { type = ModificationData.Type.CLEAR });
+                    }
+                    else
+                    {
+                        actionList.Clear();
+                    }
+                }
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    public sealed class ListEvent : ListEventBase<Action>
+    {
+        public ListEvent(bool useTryCatch = false, bool clearAfterInvoke = false) : base(useTryCatch, clearAfterInvoke) { }
+
+        public void Invoke()
+        {
+            if (!BeginInvoke())
+            {
+                return;
+            }
+
+            try
+            {
+                int count = actionList.Count;
+                if (useTryCatch)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        try
+                        {
+                            actionList[i].Invoke();
+                        }
+                        catch (Exception ex)
+                        {
+                            UnityEngine.Debug.LogException(ex);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        actionList[i].Invoke();
+                    }
+                }
+            }
+            finally
+            {
+                EndInvoke();
+            }
+        }
+        
+        public static ListEvent operator +(ListEvent listEvent, Action action)
+        {
+            listEvent.Add(action);
+            return listEvent;
+        }
+
+        public static ListEvent operator -(ListEvent listEvent, Action action)
+        {
+            listEvent.Remove(action);
+            return listEvent;
+        }
+    }
+    
+    /// <inheritdoc />
+    public sealed class ListEvent<T> : ListEventBase<Action<T>>
+    {
+        public ListEvent(bool useTryCatch = false, bool clearAfterInvoke = false) : base(useTryCatch, clearAfterInvoke) { }
+
+        public void Invoke(T t)
+        {
+            if (!BeginInvoke())
+            {
+                return;
+            }
+
+            try
+            {
+                int count = actionList.Count;
+                if (useTryCatch)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        try
+                        {
+                            actionList[i].Invoke(t);
+                        }
+                        catch (Exception ex)
+                        {
+                            UnityEngine.Debug.LogException(ex);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        actionList[i].Invoke(t);
+                    }
+                }
+            }
+            finally
+            {
+                EndInvoke();
+            }
+        }
+
+        public static ListEvent<T> operator +(ListEvent<T> listEvent, Action<T> action)
+        {
+            listEvent.Add(action);
+            return listEvent;
+        }
+
+        public static ListEvent<T> operator -(ListEvent<T> listEvent, Action<T> action)
+        {
+            listEvent.Remove(action);
+            return listEvent;
+        }
+    }
+
+    /// <inheritdoc />
+    public sealed class ListEvent<T, U> : ListEventBase<Action<T, U>>
+    {
+        public ListEvent(bool useTryCatch = false, bool clearAfterInvoke = false) : base(useTryCatch, clearAfterInvoke) { }
+
+        public void Invoke(T t, U u)
+        {
+            if (!BeginInvoke())
+            {
+                return;
+            }
+
+            try
+            {
+                int count = actionList.Count;
+                if (useTryCatch)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        try
+                        {
+                            actionList[i].Invoke(t, u);
+                        }
+                        catch (Exception ex)
+                        {
+                            UnityEngine.Debug.LogException(ex);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        actionList[i].Invoke(t, u);
+                    }
+                }
+            }
+            finally
+            {
+                EndInvoke();
+            }
+        }
+
+        public static ListEvent<T, U> operator +(ListEvent<T, U> listEvent, Action<T, U> action)
+        {
+            listEvent.Add(action);
+            return listEvent;
+        }
+
+        public static ListEvent<T, U> operator -(ListEvent<T, U> listEvent, Action<T, U> action)
+        {
+            listEvent.Remove(action);
+            return listEvent;
+        }
+    }
+
+    /// <inheritdoc />
+    public sealed class ListEvent<T, U, V> : ListEventBase<Action<T, U, V>>
+    {
+        public ListEvent(bool useTryCatch = false, bool clearAfterInvoke = false) : base(useTryCatch, clearAfterInvoke) { }
+
+        public void Invoke(T t, U u, V v)
+        {
+            if (!BeginInvoke())
+            {
+                return;
+            }
+
+            try
+            {
+                int count = actionList.Count;
+                if (useTryCatch)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        try
+                        {
+                            actionList[i].Invoke(t, u, v);
+                        }
+                        catch (Exception ex)
+                        {
+                            UnityEngine.Debug.LogException(ex);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        actionList[i].Invoke(t, u, v);
+                    }
+                }
+            }
+            finally
+            {
+                EndInvoke();
+            }
+        }
+
+        public static ListEvent<T, U, V> operator +(ListEvent<T, U, V> listEvent, Action<T, U, V> action)
+        {
+            listEvent.Add(action);
+            return listEvent;
+        }
+
+        public static ListEvent<T, U, V> operator -(ListEvent<T, U, V> listEvent, Action<T, U, V> action)
+        {
+            listEvent.Remove(action);
+            return listEvent;
+        }
+    }
+
+    /// <inheritdoc />
+    public sealed class ListEvent<T, U, V, W> : ListEventBase<Action<T, U, V, W>>
+    {
+        public ListEvent(bool useTryCatch = false, bool clearAfterInvoke = false) : base(useTryCatch, clearAfterInvoke) { }
+
+        public void Invoke(T t, U u, V v, W w)
+        {
+            if (!BeginInvoke())
+            {
+                return;
+            }
+
+            try
+            {
+                int count = actionList.Count;
+                if (useTryCatch)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        try
+                        {
+                            actionList[i].Invoke(t, u, v, w);
+                        }
+                        catch (Exception ex)
+                        {
+                            UnityEngine.Debug.LogException(ex);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        actionList[i].Invoke(t, u, v, w);
+                    }
+                }
+            }
+            finally
+            {
+                EndInvoke();
+            }
+        }
+
+        public static ListEvent<T, U, V, W> operator +(ListEvent<T, U, V, W> listEvent, Action<T, U, V, W> action)
+        {
+            listEvent.Add(action);
+            return listEvent;
+        }
+
+        public static ListEvent<T, U, V, W> operator -(ListEvent<T, U, V, W> listEvent, Action<T, U, V, W> action)
+        {
+            listEvent.Remove(action);
+            return listEvent;
+        }
+    }
+
+    /// <inheritdoc />
+    public sealed class ListEvent<T, U, V, W, X> : ListEventBase<Action<T, U, V, W, X>>
+    {
+        public ListEvent(bool useTryCatch = false, bool clearAfterInvoke = false) : base(useTryCatch, clearAfterInvoke) { }
+
+        public void Invoke(T t, U u, V v, W w, X x)
+        {
+            if (!BeginInvoke())
+            {
+                return;
+            }
+
+            try
+            {
+                int count = actionList.Count;
+                if (useTryCatch)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        try
+                        {
+                            actionList[i].Invoke(t, u, v, w, x);
+                        }
+                        catch (Exception ex)
+                        {
+                            UnityEngine.Debug.LogException(ex);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        actionList[i].Invoke(t, u, v, w, x);
+                    }
+                }
+            }
+            finally
+            {
+                EndInvoke();
+            }
+        }
+
+        public static ListEvent<T, U, V, W, X> operator +(ListEvent<T, U, V, W, X> listEvent, Action<T, U, V, W, X> action)
+        {
+            listEvent.Add(action);
+            return listEvent;
+        }
+
+        public static ListEvent<T, U, V, W, X> operator -(ListEvent<T, U, V, W, X> listEvent, Action<T, U, V, W, X> action)
+        {
+            listEvent.Remove(action);
+            return listEvent;
+        }
+    }
+    
+    /// <inheritdoc />
+    public sealed class ListEvent<T, U, V, W, X, Y> : ListEventBase<Action<T, U, V, W, X, Y>>
+    {
+        public ListEvent(bool useTryCatch = false, bool clearAfterInvoke = false) : base(useTryCatch, clearAfterInvoke) { }
+
+        public void Invoke(T t, U u, V v, W w, X x, Y y)
+        {
+            if (!BeginInvoke())
+            {
+                return;
+            }
+
+            try
+            {
+                int count = actionList.Count;
+                if (useTryCatch)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        try
+                        {
+                            actionList[i].Invoke(t, u, v, w, x, y);
+                        }
+                        catch (Exception ex)
+                        {
+                            UnityEngine.Debug.LogException(ex);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        actionList[i].Invoke(t, u, v, w, x, y);
+                    }
+                }
+            }
+            finally
+            {
+                EndInvoke();
+            }
+        }
+        
+        public static ListEvent<T, U, V, W, X, Y> operator +(ListEvent<T, U, V, W, X, Y> listEvent, Action<T, U, V, W, X, Y> action)
+        {
+            listEvent.Add(action);
+            return listEvent;
+        }
+
+        public static ListEvent<T, U, V, W, X, Y> operator -(ListEvent<T, U, V, W, X, Y> listEvent, Action<T, U, V, W, X, Y> action)
+        {
+            listEvent.Remove(action);
+            return listEvent;
+        }
+    }
+}
---
+++ Packages/com.jonagill.unitycollections/Unit Tests/ListEvent/ListEventTests.cs
@@ -0,0 +1,296 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+using UnityEngine.TestTools;
+
+namespace UnityCollections.Tests
+{
+    public class ListEventTests
+    {
+        StringBuilder sb;
+        ListEvent listEvent;
+        ListEvent autoClearListEvent;
+
+        [SetUp]
+        public void SetUp() {
+            sb = new StringBuilder();
+            listEvent = new ListEvent();
+            autoClearListEvent = new ListEvent(clearAfterInvoke: true);
+        }
+
+        private void AppendA()
+        {
+            sb.Append("A");
+        }
+
+        private void AppendB()
+        {
+            sb.Append("B");
+        }
+
+        private void AddAppendB()
+        {
+            listEvent += AppendB;
+        }
+
+        private void RemoveAppendB()
+        {
+            listEvent -= AppendB;
+        }
+
+        private void Clear()
+        {
+            listEvent.Clear();
+        }
+
+        [Test(ExpectedResult = "AB")]
+        public string AddingActionsWorks()
+        {
+            listEvent += AppendA;
+            listEvent += AppendB;
+
+            listEvent.Invoke();
+            return sb.ToString();
+        }
+
+        [Test(ExpectedResult = "ABAA")]
+        public string AddingMultipleTimesWorks()
+        {
+            listEvent += AppendA;
+            listEvent += AppendB;
+            listEvent += AppendA;
+            listEvent += AppendA;
+
+            listEvent.Invoke();
+            return sb.ToString();
+        }
+
+        [Test(ExpectedResult = "B")]
+        public string RemovingActionsWorks()
+        {
+            listEvent += AppendA;
+            listEvent += AppendB;
+            listEvent -= AppendA;
+
+            listEvent.Invoke();
+            return sb.ToString();
+        }
+
+        [Test(ExpectedResult = "")]
+        public string ClearingActionsWorks()
+        {
+            listEvent += AppendA;
+            listEvent += AppendB;
+            listEvent.Clear();
+
+            listEvent.Invoke();
+            return sb.ToString();
+        }
+
+        [Test(ExpectedResult = "")]
+        public string RemovingNonExistentActionsIsFine()
+        {
+            listEvent -= AppendA;
+
+            listEvent.Invoke();
+            return sb.ToString();
+        }
+
+        [Test(ExpectedResult = "A AB")]
+        public string AddingActionsDuringInvocationWorks()
+        {
+            listEvent += AddAppendB;
+            listEvent += AppendA;
+
+            listEvent.Invoke();
+            sb.Append(" ");
+            listEvent.Invoke();
+            return sb.ToString();
+        }
+
+        [Test(ExpectedResult = "AB A")]
+        public string RemovingActionsDuringInvocationWorks()
+        {
+            listEvent += RemoveAppendB;
+            listEvent += AppendA;
+            listEvent += AppendB;
+
+            listEvent.Invoke();
+            sb.Append(" ");
+            listEvent.Invoke();
+            return sb.ToString();
+        }
+
+        [Test(ExpectedResult = "AB ")]
+        public string ClearingActionsDuringInvocationWorks()
+        {
+            listEvent += Clear;
+            listEvent += AppendA;
+            listEvent += AppendB;
+
+            listEvent.Invoke();
+            sb.Append(" ");
+            listEvent.Invoke();
+            return sb.ToString();
+        }
+        
+        [Test(ExpectedResult = "A AB B")]
+        public string ClearingActionsAfterInvocationWorks()
+        {
+            autoClearListEvent += AppendA;
+            autoClearListEvent.Invoke();
+            
+            sb.Append(" ");
+
+            // A should fire again, then get cleared
+            autoClearListEvent += AppendA;
+            autoClearListEvent.Invoke();
+            autoClearListEvent += AppendB;
+
+            autoClearListEvent.Invoke();
+            sb.Append(" ");
+
+            autoClearListEvent += AppendB;
+            autoClearListEvent.Invoke();
+
+            // Second Invoke should not result in changes
+            autoClearListEvent.Invoke();
+            return sb.ToString();
+        }
+        
+        [Test(ExpectedResult = "ABAB A")]
+        public string ReentrancyOnInvokeWorks()
+        {
+            int reentrancyCount = 1;
+            Action reentrancy = () =>
+            {
+                reentrancyCount--;
+                if (reentrancyCount >= 0)
+                {
+                    // Clearing shouldn't take effect until after the outer Invoke returns, nor should
+                    // adding a new handler
+                    listEvent.Clear();
+                    listEvent += AppendA;
+
+                    listEvent.Invoke();
+                }
+            };
+
+            listEvent += reentrancy;
+            listEvent += AppendA;
+            listEvent += AppendB;
+            listEvent.Invoke();
+
+            sb.Append(" ");
+
+            listEvent.Invoke();
+
+            return sb.ToString();
+        }
+        
+        [TestCase(true, ExpectedResult = "[A][B] [B]")]
+        [TestCase(false, ExpectedResult = "[A] [B]")]
+        public string ZeroArgumentEventWorks(bool useTryCatch)
+        {
+            Action actionA = () => sb.AppendFormat("[A]");
+            Action actionB = () => sb.AppendFormat("[B]");
+            Action actionThrows = () => { throw new Exception("Foo"); };
+            ListEvent ev = new ListEvent(useTryCatch: useTryCatch);
+
+            ev += actionA;
+            ev += actionThrows;
+            ev += actionB;
+            InvokeExpectingException(useTryCatch, () => ev.Invoke());
+
+            sb.Append(" ");
+
+            ev -= actionA;
+            ev -= actionThrows;
+            ev.Invoke();
+
+            return sb.ToString();
+        }
+
+        [TestCase(true, ExpectedResult = "[A1][B1] [B2]")]
+        [TestCase(false, ExpectedResult = "[A1] [B2]")]
+        public string OneArgumentEventWorks(bool useTryCatch)
+        {
+            Action<int> actionA = (a) => sb.AppendFormat("[A{0}]", a);
+            Action<int> actionB = (a) => sb.AppendFormat("[B{0}]", a);
+            Action<int> actionThrows = (a) => { throw new Exception("Foo"); };
+            ListEvent<int> ev = new ListEvent<int>(useTryCatch: useTryCatch);
+
+            ev += actionA;
+            ev += actionThrows;
+            ev += actionB;
+            InvokeExpectingException(useTryCatch, () => ev.Invoke(1));
+
+            sb.Append(" ");
+
+            ev -= actionA;
+            ev -= actionThrows;
+            ev.Invoke(2);
+
+            return sb.ToString();
+        }
+
+        [TestCase(true, ExpectedResult = "[A1X][B1X] [B2Y]")]
+        [TestCase(false, ExpectedResult = "[A1X] [B2Y]")]
+        public string TwoArgumentEventWorks(bool useTryCatch)
+        {
+            Action<int, string> actionA = (a, b) => sb.AppendFormat("[A{0}{1}]", a, b);
+            Action<int, string> actionB = (a, b) => sb.AppendFormat("[B{0}{1}]", a, b);
+            Action<int, string> actionThrows = (a, b) => { throw new Exception("Foo"); };
+            ListEvent<int, string> ev = new ListEvent<int, string>(useTryCatch: useTryCatch);
+
+            ev += actionA;
+            ev += actionThrows;
+            ev += actionB;
+            InvokeExpectingException(useTryCatch, () => ev.Invoke(1, "X"));
+
+            sb.Append(" ");
+
+            ev -= actionA;
+            ev -= actionThrows;
+            ev.Invoke(2, "Y");
+
+            return sb.ToString();
+        }
+
+        [TestCase(true, ExpectedResult = "[A1X5][B1X5] [B2Y6]")]
+        [TestCase(false, ExpectedResult = "[A1X5] [B2Y6]")]
+        public string ThreeArgumentEventWorks(bool useTryCatch)
+        {
+            Action<int, string, int> actionA = (a, b, c) => sb.AppendFormat("[A{0}{1}{2}]", a, b, c);
+            Action<int, string, int> actionB = (a, b, c) => sb.AppendFormat("[B{0}{1}{2}]", a, b, c);
+            Action<int, string, int> actionThrows = (a, b, c) => { throw new Exception("Foo"); };
+            ListEvent<int, string, int> ev = new ListEvent<int, string, int>(useTryCatch: useTryCatch);
+
+            ev += actionA;
+            ev += actionThrows;
+            ev += actionB;
+            InvokeExpectingException(useTryCatch, () => ev.Invoke(1, "X", 5));
+
+            sb.Append(" ");
+
+            ev -= actionA;
+            ev -= actionThrows;
+            ev.Invoke(2, "Y", 6);
+
+            return sb.ToString();
+        }
+
+        private void InvokeExpectingException(bool useTryCatch, Action action)
+        {
+            if (useTryCatch)
+            {
+                LogAssert.Expect(UnityEngine.LogType.Exception, "Exception: Foo");
+                action.Invoke();
+            }
+            else
+            {
+                Assert.Throws<Exception>(() => action.Invoke());
+            }
+        }
+    }
+}
